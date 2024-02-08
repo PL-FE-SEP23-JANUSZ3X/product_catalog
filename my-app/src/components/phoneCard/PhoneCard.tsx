d05f07e66bcfff4d4eae966f1eb48e3699eab9fa
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-02-08 08:23:38 +0100

    feat: add PhoneCard to Page

--- my-app/src/components/phoneCard/PhoneCard.tsx
+++ my-app/src/components/phoneCard/PhoneCard.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Card, CardContent, CardMedia, Grid, Typography, } from "@mui/material";
+import { Box, Button, Card, CardContent, CardMedia, Divider, Grid, Typography } from "@mui/material";
 import { useThemeContext } from '../../theme/ThemeContext';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const breakpoints = createTheme({
   breakpoints: {
@@ -16,7 +17,6 @@ const breakpoints = createTheme({
 
 const PhoneCard = () => {
   const { theme } = useThemeContext();
-  const gray = theme.palette.elements.main
 
   return (
     <ThemeProvider theme={theme}>
@@ -27,13 +27,65 @@ const PhoneCard = () => {
           border: 1, 
           borderRadius: 0, 
           borderColor: "elements.main", 
-          p: 4,
+          boxShadow: 0,
+          p: 2,
+          [breakpoints.breakpoints.up('md')]: {
+            height: 506,
+            width: 229
+          },
+          [breakpoints.breakpoints.up('lg')]: {
+            width: 272,
+          },
         }}>        
           <CardContent>
             <CardMedia 
               component='img'
               image='/phone.svg'
+              sx={{
+                width: 223,
+                height: 130,
+                [breakpoints.breakpoints.up('md')]: {
+                  width: 165,
+                  height: 196
+                },
+              }}
             />
+            <Typography sx={{mt: 2.5, fontSize: 14}} >Apple iPhone 14 Pro 128GB Silver (MQ023)</Typography>
+            <Box sx={{ display: 'flex', mt: 1 }}>
+              <Typography variant='h6' sx={{ color: 'primary.main', fontWeight: 'bold' }}>$849</Typography>
+              <Box sx={{ width: 4 }} />
+              <Typography variant='h6' sx={{ color: 'secondary.main', textDecoration: 'line-through'}}>$1199</Typography>
+            </Box>
+            <Divider sx={{ my: 1, mb: 1.5 }} />
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 1 }}>
+              <Typography sx={{fontSize: 12, color: 'secondary.main', fontWeight: 'bold'}}>Screen</Typography>
+              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>6.5” OLED</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 1 }}>
+              <Typography sx={{fontSize: 12, color: 'secondary.main', fontWeight: 'bold'}}>Capacity</Typography>
+              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>512 GB</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 1 }}>
+              <Typography sx={{fontSize: 12, color: 'secondary.main', fontWeight: 'bold'}}>RAM</Typography>
+              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>4 GB</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2, gap: 1}}>
+              <Button variant="contained" sx={{
+                width: 176,
+                height: 40,
+                boxShadow: 0,
+                borderRadius: 0,
+                fontSize: 14,
+                fontWeight: 500,
+                textTransform: 'capitalize',
+                [breakpoints.breakpoints.up('md')]: {
+                  width: 117,
+                },
+                }}>Add to Card</Button>
+              <Button variant="outlined" sx={{minWidth: 40, height: 40, borderRadius: 0, p: 0, borderColor: 'icons.main'}}>
+                <FavoriteBorderIcon sx={{width: 20, height: 20, color: 'primary.main'}} />
+              </Button>
+            </Box>
           </CardContent>
         </Card>
       </Grid>
