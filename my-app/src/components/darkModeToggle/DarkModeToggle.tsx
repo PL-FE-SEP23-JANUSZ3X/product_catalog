bdbe64ce9d04854098f7c2b5747ec7dfcd893863
Katsiaryna Prakofyeva <catherine.smiff@gmail.com>
2024-02-09 18:42:01 +0100

    fix: linter

--- my-app/src/components/darkModeToggle/DarkModeToggle.tsx
+++ my-app/src/components/darkModeToggle/DarkModeToggle.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton } from "@mui/material";
+import { IconButton } from "@mui/material";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import { useThemeContext } from "../../theme/ThemeContext";
