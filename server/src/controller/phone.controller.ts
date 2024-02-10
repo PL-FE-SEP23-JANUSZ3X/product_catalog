d1cf36404402f3983271d4aa9cb85af5c35b837a
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-02-10 12:24:45 +0100

    feat: Add phone pagination with sorting

--- server/src/controller/phone.controller.ts
+++ server/src/controller/phone.controller.ts
@@ -1,6 +1,15 @@
 import phoneService from '../service/phone.service';
 import { ControllerAction } from '../utils/types';
 
+enum SortType {
+    NEWEST = 'newest',
+    OLDEST = 'oldest',
+    ALPHABETIC = 'alphabetic',
+    ALPHABETIC_REVERSE = 'alphabeticReverse',
+    LOWEST_PRICE = 'lowestPrice',
+    HIGHEST_PRICE = 'highestPrice'
+}
+
 const getAll: ControllerAction = async(req, res) => {
     try {
         const allPhones = await phoneService.getAllPhones();
@@ -30,30 +39,68 @@ const getOne: ControllerAction = async(req, res) => {
     }
 }
 
-const getSome: ControllerAction = async(req, res) => {
+const getSortedPhones: ControllerAction = async(req, res) => {
     try {
-        const allPhones = await phoneService.getAllPhones()
-        const reversePhones = allPhones.reverse()
+        const allPhones = await phoneService.getAllPhones();
 
-        console.log(reversePhones[0])
+        const { sortType, start, limit} = req.params
+        const { startIndex, limitIndex } = { startIndex: +start, limitIndex: +limit };
 
-        const { start, limit } = req.params;
-        
-        const length = allPhones.length;
-        
-        const endIndex = length - parseInt(start);
-        const startFromIndex = length - parseInt(limit);
-        
-        const somePhones = allPhones.slice(startFromIndex, endIndex);
-        
-        res.send(somePhones);
-        
+        let sortedPhones = []
+
+        switch (sortType) {
+            case SortType.ALPHABETIC:
+                sortedPhones = allPhones.sort((a, b) => {
+                    if (a.name && b.name) {
+                        return a.name.localeCompare(b.name)
+                    }
+                    return 0;
+                })
+            case SortType.ALPHABETIC_REVERSE:
+                sortedPhones = allPhones.sort((a, b) => {
+                    if (a.name && b.name) {
+                        return b.name.localeCompare(a.name)
+                    }
+                    return 0;
+                })
+            case SortType.NEWEST:
+                sortedPhones = allPhones.reverse()
+                break;
+            case SortType.OLDEST:
+                sortedPhones = allPhones
+                break;
+            case SortType.LOWEST_PRICE: 
+                sortedPhones = allPhones.sort((a, b) => {
+                    const priceA = a.priceDiscount ?? a.priceRegular;
+                    const priceB = b.priceDiscount ?? b.priceRegular;
+                    if (priceA && priceB) {
+                        return priceB - priceA;
+                    }
+                    return 0;
+                })
+                break;
+            case SortType.HIGHEST_PRICE: 
+                sortedPhones = allPhones.sort((a, b) => {
+                    const priceA = a.priceDiscount ?? a.priceRegular;
+                    const priceB = b.priceDiscount ?? b.priceRegular;
+                    if (priceA && priceB) {
+                        return priceB - priceA;
+                    }
+                    return 0;
+                })
+                break;
+            default:
+                sortedPhones = allPhones;
+                break;
+        }
+
+        res.json(sortedPhones.slice(startIndex, limitIndex))
     } catch (error) {
         console.log(error);
         res.status(500).send('Internal Server Error');
     }
 }
 
-const phoneController = {getAll, getOne, getSome};
+const phoneController = {getAll, getOne, getSortedPhones};
 
 export default phoneController;
--- server/src/router/phone.router.ts
+++ server/src/router/phone.router.ts
@@ -5,6 +5,6 @@ const phoneRouter = express.Router();
 
 phoneRouter.get('/', phoneController.getAll);
 phoneRouter.get('/:id', phoneController.getOne);
-phoneRouter.get('/pagination/:start-:limit', phoneController.getSome);
+phoneRouter.get('/pagination/:sortType-:start-:limit', phoneController.getSortedPhones);
 
 export default phoneRouter;
