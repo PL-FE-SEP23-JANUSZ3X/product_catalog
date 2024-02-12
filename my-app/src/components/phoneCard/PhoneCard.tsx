b33d6c96728da0c1ec86ae93beac69a16ffb7920
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-02-12 21:47:12 +0100

    update: add Styles and api data to PhoneCard

--- my-app/src/components/phoneCard/PhoneCard.tsx
+++ my-app/src/components/phoneCard/PhoneCard.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 import { Box, Button, Card, CardContent, CardMedia, Divider, Grid, Typography } from "@mui/material";
-import { useThemeContext } from '../../theme/ThemeContext';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Phone from './PhoneCard.types';
 
 const breakpoints = createTheme({
   breakpoints: {
@@ -15,92 +15,73 @@ const breakpoints = createTheme({
   },
 });
 
-const PhoneCard = () => {
-  const { theme } = useThemeContext();
-
+const PhoneCard = ( phone: Phone ) => {
+  const { images, name, priceRegular, priceDiscount, screen, capacityAvailable, ram } = phone.phone
+  console.log(screen)
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container spacing={4}>
-        <Card sx={{ 
-          width: 288, 
-          height: 440,
-          border: 1, 
-          borderRadius: 0, 
-          borderColor: "elements.main", 
-          boxShadow: 0,
-          p: 2,
-          [breakpoints.breakpoints.up('md')]: {
-            height: 506,
-            width: 229,
-          },
-          [breakpoints.breakpoints.up('lg')]: {
-            width: 272,
-          },
-        }}>        
+    <Grid>
+      <Card sx={{ 
+        width: {xs: "288px", md: "229px", lg: "272px"},
+        height: {xs: "440", md: "506px"},
+        border: 1, 
+        borderRadius: 0, 
+        borderColor: "elements.main", 
+        boxShadow: 0,
+        p: 2,
+      }}>    
           <CardContent>
             <CardMedia 
               component='img'
-              image='/phone.svg'
+              src={`https://storage.googleapis.com/image-storage-mate/${images[0]}`}
               sx={{
-                width: 223,
-                height: 130,
-                [breakpoints.breakpoints.up('md')]: {
-                  width: 165,
-                  height: 196
-                },
-                [breakpoints.breakpoints.up('lg')]: {
-                  width: 196,
-                  height: 208
-                }
+                objectFit: 'contain',
+                width: {xs: "223px", md: "165px", lg: "196px"},
+                height:{xs: "130px", md: "196px", lg: "208px"},
               }}
             />
-            <Typography sx={{mt: 2.5, fontSize: 14}} >Apple iPhone 14 Pro 128GB Silver (MQ023)</Typography>
+            <Typography sx={{mt: 2.5, fontSize: 14}} >{name}</Typography>
             <Box sx={{ display: 'flex', mt: 1 }}>
-              <Typography variant='h6' sx={{ color: 'primary.main', fontWeight: 'bold' }}>$849</Typography>
+              <Typography variant='h6' sx={{ color: 'primary.main', fontWeight: 'bold' }}>{priceRegular}</Typography>
               <Box sx={{ width: 4 }} />
-              <Typography variant='h6' sx={{ color: 'secondary.main', textDecoration: 'line-through'}}>$1199</Typography>
+              <Typography variant='h6' sx={{ color: 'secondary.main', textDecoration: 'line-through'}}>{priceDiscount}</Typography>
             </Box>
             <Divider 
               sx={{ my: 1, mb: 1.5,
                 [breakpoints.breakpoints.up('lg')]: {
                   mb: 0,
-                },}} />
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}>
+                },}} /> 
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: {xs: 0.5, md: 0.5, lg: 2} }}>
               <Typography sx={{fontSize: 12, color: 'secondary.main', fontWeight: 'bold'}}>Screen</Typography>
-              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>6.5” OLED</Typography>
+              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>{screen}</Typography>
             </Box>
             <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}>
               <Typography sx={{fontSize: 12, color: 'secondary.main', fontWeight: 'bold'}}>Capacity</Typography>
-              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>512 GB</Typography>
+              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>{capacityAvailable}</Typography>
             </Box>
             <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}>
               <Typography sx={{fontSize: 12, color: 'secondary.main', fontWeight: 'bold'}}>RAM</Typography>
-              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>4 GB</Typography>
+              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>{ram}</Typography>
             </Box>
             <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2}}>
               <Button variant="contained" sx={{
-                width: 176,
+                width: {xs: "176px", md: "117px", lg: "160px"},
                 height: 40,
                 boxShadow: 0,
                 borderRadius: 0,
                 fontSize: 14,
                 fontWeight: 500,
                 textTransform: 'capitalize',
-                [breakpoints.breakpoints.up('md')]: {
-                  width: 117,
-                },
-                [breakpoints.breakpoints.up('lg')]: {
-                  width: 160,
-                },
-                }}>Add to Card</Button>
+                }}
+              >
+                Add to Card
+              </Button>
               <Button variant="outlined" sx={{minWidth: 40, height: 40, borderRadius: 0, p: 0, borderColor: 'icons.main'}}>
                 <FavoriteBorderIcon sx={{width: 20, height: 20, color: 'primary.main'}} />
               </Button>
             </Box>
           </CardContent>
         </Card>
-      </Grid>
-    </ThemeProvider>
+    </Grid>
   )
 }
 
