a62b6abe0296e2093bf78a1f86ff4a08ec4aae68
jacekkosiba <jac.kosiba@gmail.com>
2024-02-14 17:18:58 +0100

    feat: replace getOne controller with getById controller and /phones/:id route by /phones/:phoneId route

--- server/src/controller/phone.controller.ts
+++ server/src/controller/phone.controller.ts
@@ -2,98 +2,103 @@ import phoneService from '../service/phone.service';
 import SortType from '../types/sortType';
 import { ControllerAction } from '../utils/types';
 
-const getAll: ControllerAction = async(req, res) => {
-    try {
-        const allPhones = await phoneService.getAllPhones();
+const getAll: ControllerAction = async (req, res) => {
+  try {
+    const allPhones = await phoneService.getAllPhones();
 
-        if (!allPhones) {
-            res.status(404).send('Not Found: The specified entity does not exist');
+    if (!allPhones) {
+      res.status(404).send('Not Found: The specified entity does not exist');
 
-            return;
-        }
-        res.send(allPhones);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Internal Server Error');
+      return;
     }
-}
-
-const getOne: ControllerAction = async(req, res) => {
-    try {
-        const allPhones = await phoneService.getAllPhones()
-
-        const { id } = req.params
-
-        res.send(allPhones[+id])
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Internal Server Error');
-    }
-}
-
-const getSortedPhones: ControllerAction = async(req, res) => {
-    try {
-        const allPhones = await phoneService.getAllPhones();
-
-        const { sortType, start, limit } = req.params
-        const { startIndex, limitIndex } = { startIndex: +start, limitIndex: +limit };
-
-        let sortedPhones = []
-
-        switch (sortType) {
-            case SortType.ALPHABETIC:
-                sortedPhones = allPhones.sort((a, b) => {
-                    if (a.name && b.name) {
-                        return a.name.localeCompare(b.name)
-                    }
-                    return 0;
-                })
-                break;
-            case SortType.NEWEST:
-                sortedPhones = allPhones.slice(0, limitIndex); 
-                break;
-            case SortType.OLDEST:
-                sortedPhones = allPhones.slice(startIndex); 
-                break;
-            case SortType.ALPHABETIC_REVERSE:
-                sortedPhones = allPhones.sort((a, b) => {
-                    if (a.name && b.name) {
-                        return b.name.localeCompare(a.name);
-                    }
-                    return 0;
-                });
-                break;
-            case SortType.CHEAPEST:
-                sortedPhones = allPhones.sort((a, b) => {
-                    const priceA = a.priceDiscount ?? a.priceRegular;
-                    const priceB = b.priceDiscount ?? b.priceRegular;
-                    if (priceA && priceB) {
-                        return priceA - priceB;
-                    }
-                    return 0;
-                });
-                break;
-            case SortType.EXPENSIVE:
-                sortedPhones = allPhones.sort((a, b) => {
-                    const priceA = a.priceDiscount ?? a.priceRegular;
-                    const priceB = b.priceDiscount ?? b.priceRegular;
-                    if (priceA && priceB) {
-                        return priceB - priceA;
-                    }
-                    return 0;
-                });
-                break;
-            default:
-                sortedPhones = allPhones;
-                break;
-        }
-
-        res.json(sortedPhones.slice(startIndex, limitIndex))
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Internal Server Error');
-    }
+    res.send(allPhones);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error');
+  }
 };
-const phoneController = {getAll, getOne, getSortedPhones};
+
+const getById: ControllerAction = async (req, res) => {
+  try {
+    const allPhones = await phoneService.getAllPhones();
+
+    const { phoneId } = req.params;
+
+    const phone = allPhones.find((phone) => phone.id === phoneId);
+
+    res.send(phone);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+const getSortedPhones: ControllerAction = async (req, res) => {
+  try {
+    const allPhones = await phoneService.getAllPhones();
+
+    const { sortType, start, limit } = req.params;
+    const { startIndex, limitIndex } = {
+      startIndex: +start,
+      limitIndex: +limit,
+    };
+
+    let sortedPhones = [];
+
+    switch (sortType) {
+      case SortType.ALPHABETIC:
+        sortedPhones = allPhones.sort((a, b) => {
+          if (a.name && b.name) {
+            return a.name.localeCompare(b.name);
+          }
+          return 0;
+        });
+        break;
+      case SortType.NEWEST:
+        sortedPhones = allPhones.slice(0, limitIndex);
+        break;
+      case SortType.OLDEST:
+        sortedPhones = allPhones.slice(startIndex);
+        break;
+      case SortType.ALPHABETIC_REVERSE:
+        sortedPhones = allPhones.sort((a, b) => {
+          if (a.name && b.name) {
+            return b.name.localeCompare(a.name);
+          }
+          return 0;
+        });
+        break;
+      case SortType.CHEAPEST:
+        sortedPhones = allPhones.sort((a, b) => {
+          const priceA = a.priceDiscount ?? a.priceRegular;
+          const priceB = b.priceDiscount ?? b.priceRegular;
+          if (priceA && priceB) {
+            return priceA - priceB;
+          }
+          return 0;
+        });
+        break;
+      case SortType.EXPENSIVE:
+        sortedPhones = allPhones.sort((a, b) => {
+          const priceA = a.priceDiscount ?? a.priceRegular;
+          const priceB = b.priceDiscount ?? b.priceRegular;
+          if (priceA && priceB) {
+            return priceB - priceA;
+          }
+          return 0;
+        });
+        break;
+      default:
+        sortedPhones = allPhones;
+        break;
+    }
+
+    res.json(sortedPhones.slice(startIndex, limitIndex));
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+const phoneController = { getAll, getSortedPhones, getById };
 
 export default phoneController;
--- server/src/router/phone.router.ts
+++ server/src/router/phone.router.ts
@@ -4,7 +4,10 @@ import phoneController from '../controller/phone.controller';
 const phoneRouter = express.Router();
 
 phoneRouter.get('/', phoneController.getAll);
-phoneRouter.get('/:id', phoneController.getOne);
-phoneRouter.get('/pagination/:sortType-:start-:limit-:order', phoneController.getSortedPhones);
+phoneRouter.get(
+  '/pagination/:sortType-:start-:limit-:order',
+  phoneController.getSortedPhones,
+);
+phoneRouter.get('/:phoneId', phoneController.getById);
 
 export default phoneRouter;
