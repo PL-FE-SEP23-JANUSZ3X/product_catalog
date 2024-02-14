92b955ce61038d21f49c34dd40b4fff09b6b8401
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-02-14 16:31:08 +0100

    Implemet: add title and slider to HomePage

--- my-app/src/components/pages/HomePage.tsx
+++ my-app/src/components/pages/HomePage.tsx
@@ -1,14 +1,39 @@
-import { SxProps, Typography } from "@mui/material";
+import { Container, SxProps, Typography, useMediaQuery } from "@mui/material";
+import '../../App.css'
+import Slider from '../slider/Slider'
 
 export type CopyrightProps = {
   sx: SxProps;
 };
 
+const BANNER_IMAGES = [
+  '/img/banner/banner-page.png',
+  '/img/banner/banner-phones.png',
+  '/img/banner/banner-tablets.png'
+]
+
+const MOBILE_BANNER_IMAGES = [
+  '/img/banner/banner-page-mobile.png',
+  '/img/banner/banner-phones.png',
+  '/img/banner/banner-tablets.png'
+]
+
 const HomePage = () => {
 
-  return (
-    <Typography variant="h1" color="text.primary" mb="32px" >HomePage</Typography>
+  const isMobile = useMediaQuery(theme => theme.breakpoints.down('sm'));
 
+  const images = isMobile ? MOBILE_BANNER_IMAGES : BANNER_IMAGES;
+
+  return (
+    <Container sx={{padding: 0}}>
+      <Typography variant="h1" sx={
+        {
+          my: { xs: '32px', md: '52px' },
+          fontSize: 32,
+          lineHeight: '41px',
+        }}>Welcome to Nice Gadgets store!</Typography>
+      <Slider images={images} />
+    </ Container>
   );
 };
 
