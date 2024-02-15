fe4217cfa9fe4ec7e8b37f9fdbad980543dd289a
Timi2424 <tymonbieganski@wp.pl>
2024-02-15 14:58:34 +0100

    fixing seeder

--- server/seeders/20240208122627-Phone.js
+++ server/seeders/20240208122627-Phone.js
@@ -1,25 +1,26 @@
 'use strict';
 
+const phoneData = require('../src/apiData/phones.json');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const transformedPhoneData = phoneData.map(phone => ({
+      ...phone,
+      capacityAvailable: phone.capacityAvailable,
+      colorsAvailable: phone.colorsAvailable,
+      images: phone.images,
+      description: JSON.stringify(phone.description),
+      cell: phone.cell,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }));
+
+    await queryInterface.bulkInsert('Phones', transformedPhoneData, {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+
+    await queryInterface.bulkDelete('Phones', null, {});
   }
 };
