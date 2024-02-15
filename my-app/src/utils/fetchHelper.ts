91f9e1936cd1ffb180e8b7bee7b18eaf2a22fc8d
Timi2424 <tymonbieganski@wp.pl>
2024-02-15 10:54:56 +0100

    Ui-basic-layout-for-phone-page

--- my-app/src/utils/fetchHelper.ts
+++ my-app/src/utils/fetchHelper.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import 'dotenv/config';
 
-const BASE_URL = process.env.API;
+const BASE_URL = process.env.REACT_APP_API;
 
 export const getPhones = async () => {
   const response = await axios.get(`${BASE_URL}/phones`);
