54a7852822a7cd269637ea9ac2517e7f878ee293
jacekkosiba <jac.kosiba@gmail.com>
2024-02-16 16:18:31 +0100

    feat: itemCard is a link that routes to appropriate product now

--- my-app/src/components/itemCard/ItemCard.tsx
+++ my-app/src/components/itemCard/ItemCard.tsx
@@ -1,64 +1,136 @@
-import { Box, Button, Card, CardContent, CardMedia, Divider, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  Typography,
+} from '@mui/material';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import Item from './ItemCard.types';
-import { useOrderContext } from "../../context/useOrderContext";
+import { useOrderContext } from '../../context/useOrderContext';
+import { Link as RouterLink } from 'react-router-dom';
 
-const ItemCard = ( item: Item ) => {
-  const { id, images, name, priceRegular, priceDiscount, screen, capacityAvailable, ram } = item.item
-  
+const ItemCard = (item: Item) => {
   const {
-    addToOrder,
-  } = useOrderContext()
+    id,
+    images,
+    name,
+    priceRegular,
+    priceDiscount,
+    screen,
+    capacityAvailable,
+    ram,
+  } = item.item;
 
-  const handleAddToOrder = (id: string, priceRegular: number) => () => addToOrder(id, priceRegular);
+  const { addToOrder } = useOrderContext();
+
+  const handleAddToOrder = (id: string, priceRegular: number) => () =>
+    addToOrder(id, priceRegular);
 
   return (
-    <Grid>
-      <Card sx={{ 
-        width: {xs: "288px", md: "229px", lg: "272px"},
-        height: {xs: "440", md: "506px"},
-        border: 1, 
-        borderRadius: 0, 
-        borderColor: "elements.main", 
-        boxShadow: 0,
-        p: 2,
-      }}>    
+    <RouterLink to={`/phones/${id}`}>
+      <Grid>
+        <Card
+          sx={{
+            width: { xs: '288px', md: '229px', lg: '272px' },
+            height: { xs: '440', md: '506px' },
+            border: 1,
+            borderRadius: 0,
+            borderColor: 'elements.main',
+            boxShadow: 0,
+            p: 2,
+          }}
+        >
           <CardContent>
-            <CardMedia 
-              component='img'
+            <CardMedia
+              component="img"
               src={images[0]}
               sx={{
                 objectFit: 'contain',
-                width: {xs: "223px", md: "165px", lg: "196px"},
-                height:{xs: "130px", md: "196px", lg: "208px"},
+                width: { xs: '223px', md: '165px', lg: '196px' },
+                height: { xs: '130px', md: '196px', lg: '208px' },
               }}
             />
-            <Typography sx={{mt: 2.5, fontSize: 14}} >{name}</Typography>
+            <Typography sx={{ mt: 2.5, fontSize: 14 }}>{name}</Typography>
             <Box sx={{ display: 'flex', mt: 1 }}>
-              <Typography variant='h6' sx={{ color: 'primary.main', fontWeight: 'bold' }}>{priceRegular}</Typography>
+              <Typography
+                variant="h6"
+                sx={{ color: 'primary.main', fontWeight: 'bold' }}
+              >
+                {priceRegular}
+              </Typography>
               <Box sx={{ width: 4 }} />
-              <Typography variant='h6' sx={{ color: 'secondary.main', textDecoration: 'line-through'}}>{priceDiscount}</Typography>
+              <Typography
+                variant="h6"
+                sx={{ color: 'secondary.main', textDecoration: 'line-through' }}
+              >
+                {priceDiscount}
+              </Typography>
             </Box>
-            <Divider 
-              sx={{ my: 1, mb: 1.5}} /> 
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: {xs: 0.5, md: 0.5, lg: 2} }}>
-              <Typography sx={{fontSize: 12, color: 'secondary.main', fontWeight: 'bold'}}>Screen</Typography>
-              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>{screen}</Typography>
+            <Divider sx={{ my: 1, mb: 1.5 }} />
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                mt: { xs: 0.5, md: 0.5, lg: 2 },
+              }}
+            >
+              <Typography
+                sx={{
+                  fontSize: 12,
+                  color: 'secondary.main',
+                  fontWeight: 'bold',
+                }}
+              >
+                Screen
+              </Typography>
+              <Typography sx={{ fontSize: 12, fontWeight: 'bold' }}>
+                {screen}
+              </Typography>
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}>
-              <Typography sx={{fontSize: 12, color: 'secondary.main', fontWeight: 'bold'}}>Capacity</Typography>
-              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>{capacityAvailable}</Typography>
+            <Box
+              sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}
+            >
+              <Typography
+                sx={{
+                  fontSize: 12,
+                  color: 'secondary.main',
+                  fontWeight: 'bold',
+                }}
+              >
+                Capacity
+              </Typography>
+              <Typography sx={{ fontSize: 12, fontWeight: 'bold' }}>
+                {capacityAvailable}
+              </Typography>
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}>
-              <Typography sx={{fontSize: 12, color: 'secondary.main', fontWeight: 'bold'}}>RAM</Typography>
-              <Typography sx={{fontSize: 12, fontWeight: 'bold'}}>{ram}</Typography>
+            <Box
+              sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}
+            >
+              <Typography
+                sx={{
+                  fontSize: 12,
+                  color: 'secondary.main',
+                  fontWeight: 'bold',
+                }}
+              >
+                RAM
+              </Typography>
+              <Typography sx={{ fontSize: 12, fontWeight: 'bold' }}>
+                {ram}
+              </Typography>
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2}}>
+            <Box
+              sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}
+            >
               <Button
-                component='button'
+                component="button"
                 variant="contained"
                 sx={{
-                  width: {xs: "176px", md: "117px", lg: "160px"},
+                  width: { xs: '176px', md: '117px', lg: '160px' },
                   height: 40,
                   boxShadow: 0,
                   borderRadius: 0,
@@ -70,14 +142,26 @@ const ItemCard = ( item: Item ) => {
               >
                 Add to Card
               </Button>
-              <Button variant="outlined" sx={{minWidth: 40, height: 40, borderRadius: 0, p: 0, borderColor: 'icons.main'}}>
-                <FavoriteBorderIcon sx={{width: 20, height: 20, color: 'primary.main'}} />
+              <Button
+                variant="outlined"
+                sx={{
+                  minWidth: 40,
+                  height: 40,
+                  borderRadius: 0,
+                  p: 0,
+                  borderColor: 'icons.main',
+                }}
+              >
+                <FavoriteBorderIcon
+                  sx={{ width: 20, height: 20, color: 'primary.main' }}
+                />
               </Button>
             </Box>
           </CardContent>
         </Card>
-    </Grid>
-  )
-}
+      </Grid>
+    </RouterLink>
+  );
+};
 
 export default ItemCard;
