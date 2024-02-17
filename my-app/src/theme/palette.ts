4a9d74c056f0e790091fff8e4f536ad96d4164c6
Katsiaryna Prakofyeva <catherine.smiff@gmail.com>
2024-02-17 22:57:11 +0100

    split theme file

---
+++ my-app/src/theme/palette.ts
@@ -0,0 +1,92 @@
+export const lightPalette = {
+    // palette values for light mode
+    primary: {
+    main: "#313237",
+    },
+    secondary: {
+    main: "#89939A",
+    },
+    icons: {
+    main: "#B4BDC3",
+    },
+    elements: {
+    main: "#E2E6E9",
+    },
+    bgHover: {
+    main: "#FAFBFC",
+    },
+    white: {
+    main: "#fff",
+    },
+    green: {
+    main: "#27AE60",
+    },
+    red: {
+    main: "#EB5757",
+    },
+    accent: {
+    main: "#313237",
+    },
+    background: {
+    default: "#fff",
+    paper: "#fff",
+    },
+    text: {
+    primary: "#313237",
+    secondary: "#fff",
+    },
+    success: {
+    main: "#27AE60",
+    },
+    badge: {
+    main: "#fff"
+    },
+    badgeBorder: {
+    main: "#fff"
+    }
+}
+
+export const darkPalette = {
+    // palette values for dark mode
+    primary: {
+      main: "#F1F2F9",
+    },
+    secondary: {
+      main: "#75767F",
+    },
+    icons: {
+      main: "#4A4D58",
+    },
+    elements: {
+      main: "#3B3E4A",
+    },
+    bgHover: {
+      main: "#3B3E4A",
+    },
+    white: {
+      main: "#F1F2F9",
+    },
+    green: {
+      main: "#27AE60",
+    },
+    red: {
+      main: "#EB5757",
+    },
+    accent: {
+      main: "#905BFF",
+    },
+    background: {
+      default: "#0F1121",
+      paper: "#161827",
+    },
+    text: {
+      primary: "#F1F2F9",
+      secondary: "#75767F",
+    },
+    badge: {
+      main: "#fff"
+    },
+    badgeBorder: {
+      main: "#323542"
+    }
+}
---
+++ my-app/src/theme/typography.ts
@@ -0,0 +1,73 @@
+export const typography = {
+    fontFamily: ["Mont", "sans-serif"].join(","),
+    h1: {
+      fontWeight: 800,
+      fontSize: "48px",
+      lineHeight: "56px",
+    },
+    h1Mob: {
+      fontWeight: 800,
+      fontSize: "32px",
+      lineHeight: "41px",
+    },
+    h2: {
+      fontWeight: 800,
+      fontSize: "32px",
+      lineHeight: "41px",
+    },
+    h2Mob: {
+      fontWeight: 800,
+      fontSize: "22px",
+      lineHeight: "31px",
+    },
+    h3: {
+      fontWeight: 800,
+      fontSize: "22px",
+      lineHeight: "30px",
+    },
+    h3Mob: {
+      fontWeight: 700,
+      fontSize: "20px",
+      lineHeight: "26px",
+    },
+    h4: {
+      fontWeight: 700,
+      fontSize: "20px",
+      lineHeight: "26px",
+    },
+    h4Mob: {
+      fontWeight: 700,
+      fontSize: "16px",
+      lineHeight: "20px",
+    },
+    button: {
+      fontWeight: 600,
+      fontSize: "14px",
+      lineHeight: "21px",
+      textTransform: "uppercase" as const,
+      textDecoration: 'none',
+    },
+    body1: {
+      fontWeight: 500,
+      fontSize: "14px",
+      lineHeight: "21px",
+    },
+    body2: {
+      fontWeight: 600,
+      fontSize: "12px",
+      lineHeight: "16px",
+    },
+    upper: {
+      fontWeight: 700,
+      fontSize: "12px",
+      lineHeight: "11px",
+      textTransform: "uppercase" as const,
+    },
+    link: {
+      fontSize: '12px',
+      fontWeight: '800',
+      lineHeight: '11px',
+      letterSpacing: '0.04em',
+      textTransform: "uppercase" as const,
+    },
+}
