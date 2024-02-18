6a28592f5b24a1dfa5f7a520f3a782147be30074
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-02-18 13:55:37 +0100

    feat: Add navigation to section

--- my-app/src/components/pages/HomePage.tsx
+++ my-app/src/components/pages/HomePage.tsx
@@ -1,10 +1,11 @@
-import { Box, CardMedia, Container, SxProps, Typography, useMediaQuery } from "@mui/material";
+import { Box, CardMedia, Container, Link, SxProps, Typography, useMediaQuery } from "@mui/material";
 import Slider from '../slider/Slider'
 import Section from "../section/Section";
 import Carousel from "../carousel/Carousel";
 import { useEffect, useState } from "react";
 import Item from "../itemCard/ItemCard.types";
 import { useThemeContext } from "../../theme/ThemeContext";
+import { NavLink } from "react-router-dom";
 
 export type CopyrightProps = {
   sx: SxProps;
@@ -64,48 +65,60 @@ const HomePage = () => {
       <Section>
         <Typography variant="h2" sx={{fontSize: { xs: '24px', sm: '32px' }, mb: '24px'}}>Shop by category</Typography>
         <Box sx={{ display: 'flex', flexWrap: ['wrap', 'nowrap'], gap: '16px' }}>
-          <Box 
-            sx={{width: { xs: '288px', sm: '187px', md: '368px' },
-              height: { xs: '288px', sm: '187px', md: '368px' },
-              position: 'relative', backgroundColor: '#6D6474', overflow: 'hidden'
-            }}
-          >
-            <CardMedia
-              component="img"
-              src="img/categories/category-phone.png"
-              sx={{position:'relative', left: { xs: '32px', sm: '24px', md: '21px' }, top: { xs: '35px', sm: '20px', md: '40px' },
-              width: { xs: '302.09px', sm: '196.15px', md: '386px' },
-              height: { xs: '357.65px', sm: '232.23px', md: '457px' }}}
-            />
-          </Box>
-          <Box 
-            sx={{width: { xs: '288px', sm: '187px', md: '368px' },
-              height: { xs: '288px', sm: '187px', md: '368px' },
-              position: 'relative', backgroundColor: '#6D6474', overflow: 'hidden'
-            }}
-          >
-            <CardMedia
-              component="img"
-              src="img/categories/category-tablets.png"
-              sx={{position:'relative', left: { xs: '17px', sm: '10px', md: '49px' }, top: { xs: '23.1px', sm: '20px', md: '40px' },
-              width: { xs: '427.3px', sm: '277.45px', md: '546px' },
-              height: { xs: '427.3px', sm: '277.45px', md: '546px' }}}
-            />
-          </Box>
-          <Box 
-            sx={{width: { xs: '288px', sm: '187px', md: '368px' },
-              height: { xs: '288px', sm: '187px', md: '368px' },
-              position: 'relative', backgroundColor: '#6D6474', overflow: 'hidden'
-            }}
-          >
-            <CardMedia
-              component="img"
-              src="img/categories/category-accesories.png"
-              sx={{position:'relative', left: { xs: '-289.65px', sm: '-189px', md: '-368px' }, top: { xs: '-355.9px', sm: '-218px', md: '-439px' },
-              width: { xs: '1152px', sm: '748px', md: '1472px' },
-              height: { xs: '688.7px', sm: '447.17px', md: '880px' }}}
-            />  
-          </Box>
+          <NavLink to={"/phones"} style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Box 
+              sx={{width: { xs: '288px', sm: '187px', md: '368px' },
+                height: { xs: '288px', sm: '187px', md: '368px' },
+                position: 'relative', backgroundColor: '#6D6474', overflow: 'hidden'
+              }}
+            >
+              <CardMedia
+                component="img"
+                src="img/categories/category-phone.png"
+                sx={{position:'relative', left: { xs: '32px', sm: '24px', md: '21px' }, top: { xs: '35px', sm: '20px', md: '40px' },
+                width: { xs: '302.09px', sm: '196.15px', md: '386px' },
+                height: { xs: '357.65px', sm: '232.23px', md: '457px' }}}
+              />
+            </Box>
+            <Typography variant='h4' sx={{mt: '24px'}}>Mobile phones</Typography>
+            <Typography variant='body1' sx={{mt: '4px', color: 'secondary.main'}}>95 models</Typography>
+          </NavLink>
+          <NavLink to={"/tablets"} style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Box 
+              sx={{width: { xs: '288px', sm: '187px', md: '368px' },
+                height: { xs: '288px', sm: '187px', md: '368px' },
+                position: 'relative', backgroundColor: '#6D6474', overflow: 'hidden'
+              }}
+            >
+              <CardMedia
+                component="img"
+                src="img/categories/category-tablets.png"
+                sx={{position:'relative', left: { xs: '17px', sm: '10px', md: '49px' }, top: { xs: '23.1px', sm: '20px', md: '40px' },
+                width: { xs: '427.3px', sm: '277.45px', md: '546px' },
+                height: { xs: '427.3px', sm: '277.45px', md: '546px' }}}
+              />
+            </Box>
+            <Typography variant='h4' sx={{mt: '24px'}}>Tablets</Typography>
+            <Typography variant='body1' sx={{mt: '4px', color: 'secondary.main'}}>24 models</Typography>
+          </NavLink>
+          <NavLink to={"/accessories"} style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Box 
+              sx={{width: { xs: '288px', sm: '187px', md: '368px' },
+                height: { xs: '288px', sm: '187px', md: '368px' },
+                position: 'relative', backgroundColor: '#6D6474', overflow: 'hidden'
+              }}
+            >
+              <CardMedia
+                component="img"
+                src="img/categories/category-accesories.png"
+                sx={{position:'relative', left: { xs: '-289.65px', sm: '-189px', md: '-368px' }, top: { xs: '-355.9px', sm: '-218px', md: '-439px' },
+                width: { xs: '1152px', sm: '748px', md: '1472px' },
+                height: { xs: '688.7px', sm: '447.17px', md: '880px' }}}
+              />  
+            </Box>
+            <Typography variant='h4' sx={{mt: '24px'}}>Accessories</Typography>
+            <Typography variant='body1' sx={{mt: '4px', color: 'secondary.main'}}>100 models</Typography>
+          </NavLink>
         </Box>
       </Section>
     </>
