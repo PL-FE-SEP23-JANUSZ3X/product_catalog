1fcd41527f18f2e4ec32e5c4575885fc3d6ba029
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-02-18 13:34:06 +0100

    feat: Add categories sections

--- my-app/src/components/itemCard/ItemCard.tsx
+++ my-app/src/components/itemCard/ItemCard.tsx
@@ -1,9 +1,25 @@
+import React from 'react';
 import { Grid, Card, CardContent, CardMedia, Typography, Button, Box, Divider } from "@mui/material";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import Item from './ItemCard.types';
+import { useOrderContext } from '../../context/useOrderContext';
+import { Link as RouterLink } from 'react-router-dom';
 
 const ItemCard = ({ item, carouselWidth }: { item: Item, carouselWidth?: string }) => {
-  const { images, name, priceRegular, priceDiscount, screen, capacityAvailable, ram } = item;
+  const { id, images, name, priceRegular, priceDiscount, screen, capacityAvailable, ram } = item;
+  const { addToOrder } = useOrderContext();
+
+  const handleAddToOrder = (id: string, priceRegular: number) => (event: React.MouseEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    addToOrder(id, priceRegular);
+  };
+
+  const handleAddToFavorites = (id: string) => (event: React.MouseEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(`add ${id} to favorites`);
+  };
 
   const cardStyle = {
     width: typeof carouselWidth === 'string' ? carouselWidth : { xs: "288px", sm: "229px", md: "272px" },
@@ -16,60 +32,65 @@ const ItemCard = ({ item, carouselWidth }: { item: Item, carouselWidth?: string 
   };
 
   return (
-    <Grid item>
-      <Card sx={cardStyle}>
-        <CardContent>
-          <CardMedia
-            component='img'
-            src={images[0]}
-            sx={{
-              objectFit: 'contain',
-              display: 'flex',
-              justifyContent: 'center',
-              width: typeof carouselWidth === 'string' ? "148px" : { xs: "223px", sm: "165px", md: "196px" },
-              height: { xs: "130px", sm: "196px", md: "208px" },
-            }}
-          />
-          <Typography sx={{ mt: 2.5, fontSize: 14 }}>{name}</Typography>
-          <Box sx={{ display: 'flex', mt: 1 }}>
-            <Typography variant='h6' sx={{ color: 'primary.main', fontWeight: 'bold' }}>{`${priceRegular !== undefined ? priceRegular : ''}$`}</Typography>
-            <Box sx={{ width: 4 }} />
-            <Typography variant='h6' sx={{ color: 'secondary.main', textDecoration: 'line-through' }}>{`${priceDiscount !== undefined ? priceDiscount : ''}$`}</Typography>
-          </Box>
-          <Divider sx={{ my: 1, mb: 1.5 }} />
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: { xs: 0.5, sm: 0.5, md: 2 } }}>
-            <Typography sx={{ fontSize: 12, color: 'secondary.main', fontWeight: 'bold' }}>Screen</Typography>
-            <Typography sx={{ fontSize: 12, fontWeight: 'bold' }}>{screen !== undefined ? screen : ''}</Typography>
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}>
-            <Typography sx={{ fontSize: 12, color: 'secondary.main', fontWeight: 'bold' }}>Capacity</Typography>
-            <Typography sx={{ fontSize: 12, fontWeight: 'bold' }}>{capacityAvailable !== undefined ? capacityAvailable : ''}</Typography>
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}>
-            <Typography sx={{ fontSize: 12, color: 'secondary.main', fontWeight: 'bold' }}>RAM</Typography>
-            <Typography sx={{ fontSize: 12, fontWeight: 'bold' }}>{ram !== undefined ? ram : ''}</Typography>
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
-            <Button variant="contained" sx={{
-              width: typeof carouselWidth === 'string' ? "400px" : { xs: "176px", sm: "117px", md: "160px" },
-              height: 40,
-              boxShadow: 0,
-              borderRadius: 0,
-              fontSize: 12,
-              fontWeight: 500,
-              textTransform: 'capitalize',
-            }}
-            >
-              <Typography sx={{fontSize: { xs: "11px", sm: "13px" }, fontWeight: 700 }} >Add to Card</Typography>
-            </Button>
-            <Button variant="outlined" sx={{ minWidth: 40, height: 40, borderRadius: 0, p: 0, borderColor: 'icons.main' }}>
-              <FavoriteBorderIcon sx={{ width: 20, height: 20, color: 'primary.main' }} />
-            </Button>
-          </Box>
-        </CardContent>
-      </Card>
-    </Grid>
-  )
-}
+    <RouterLink to={`/phones/${id}`} style={{ textDecoration: 'none' }}>
+      <Grid item>
+        <Card sx={cardStyle}>
+          <CardContent>
+            <CardMedia
+              component="img"
+              src={images[0]}
+              sx={{
+                objectFit: 'contain',
+                display: 'flex',
+                justifyContent: 'center',
+                width: typeof carouselWidth === 'string' ? "148px" : { xs: "223px", sm: "165px", md: "196px" },
+                height: { xs: "130px", sm: "196px", md: "208px" },
+              }}
+            />
+            <Typography sx={{ mt: 2.5, fontSize: 14 }}>{name}</Typography>
+            <Box sx={{ display: 'flex', mt: 1 }}>
+              <Typography variant='h6' sx={{ color: 'primary.main', fontWeight: 'bold' }}>{`${priceRegular !== undefined ? priceRegular : ''}$`}</Typography>
+              <Box sx={{ width: 4 }} />
+              <Typography variant='h6' sx={{ color: 'secondary.main', textDecoration: 'line-through' }}>{`${priceDiscount !== undefined ? priceDiscount : ''}$`}</Typography>
+            </Box>
+            <Divider sx={{ my: 1, mb: 1.5 }} />
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: { xs: 0.5, sm: 0.5, md: 2 } }}>
+              <Typography sx={{ fontSize: 12, color: 'secondary.main', fontWeight: 'bold' }}>Screen</Typography>
+              <Typography sx={{ fontSize: 12, fontWeight: 'bold' }}>{screen !== undefined ? screen : ''}</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}>
+              <Typography sx={{ fontSize: 12, color: 'secondary.main', fontWeight: 'bold' }}>Capacity</Typography>
+              <Typography sx={{ fontSize: 12, fontWeight: 'bold' }}>{capacityAvailable !== undefined ? capacityAvailable : ''}</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 0.5 }}>
+              <Typography sx={{ fontSize: 12, color: 'secondary.main', fontWeight: 'bold' }}>RAM</Typography>
+              <Typography sx={{ fontSize: 12, fontWeight: 'bold' }}>{ram !== undefined ? ram : ''}</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
+              <Button variant="contained" sx={{
+                width: typeof carouselWidth === 'string' ? "400px" : { xs: "176px", sm: "117px", md: "160px" },
+                height: 40,
+                boxShadow: 0,
+                borderRadius: 0,
+                fontSize: 12,
+                fontWeight: 500,
+                textTransform: 'capitalize',
+              }}
+                onClick={(event) => handleAddToOrder(id, priceRegular)(event)}
+              >
+                Add to Cart
+              </Button>
+              <Button variant="outlined" sx={{ minWidth: 40, height: 40, borderRadius: 0, p: 0, borderColor: 'icons.main' }}
+                onClick={(event) => handleAddToFavorites(id)(event)}
+              >
+                <FavoriteBorderIcon sx={{ width: 20, height: 20, color: 'primary.main' }} />
+              </Button>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+    </RouterLink>
+  );
+};
 
 export default ItemCard;
--- my-app/src/components/pages/HomePage.tsx
+++ my-app/src/components/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Container, SxProps, Typography, useMediaQuery } from "@mui/material";
+import { Box, CardMedia, Container, SxProps, Typography, useMediaQuery } from "@mui/material";
 import Slider from '../slider/Slider'
 import Section from "../section/Section";
 import Carousel from "../carousel/Carousel";
@@ -29,7 +29,7 @@ const HomePage = () => {
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const images = isMobile ? MOBILE_BANNER_IMAGES : BANNER_IMAGES;
 
-  const newModelScectionTitle = 'Brand new models'
+  const newModelSectionTitle = 'Brand new models'
 
   useEffect(() => {
     const fetchData = async () => {
@@ -59,7 +59,54 @@ const HomePage = () => {
         <Slider images={images} />
       </Container>
       <Section>
-        <Carousel title={newModelScectionTitle} products={newModels} />
+        <Carousel title={newModelSectionTitle} products={newModels} />
+      </Section>
+      <Section>
+        <Typography variant="h2" sx={{fontSize: { xs: '24px', sm: '32px' }, mb: '24px'}}>Shop by category</Typography>
+        <Box sx={{ display: 'flex', flexWrap: ['wrap', 'nowrap'], gap: '16px' }}>
+          <Box 
+            sx={{width: { xs: '288px', sm: '187px', md: '368px' },
+              height: { xs: '288px', sm: '187px', md: '368px' },
+              position: 'relative', backgroundColor: '#6D6474', overflow: 'hidden'
+            }}
+          >
+            <CardMedia
+              component="img"
+              src="img/categories/category-phone.png"
+              sx={{position:'relative', left: { xs: '32px', sm: '24px', md: '21px' }, top: { xs: '35px', sm: '20px', md: '40px' },
+              width: { xs: '302.09px', sm: '196.15px', md: '386px' },
+              height: { xs: '357.65px', sm: '232.23px', md: '457px' }}}
+            />
+          </Box>
+          <Box 
+            sx={{width: { xs: '288px', sm: '187px', md: '368px' },
+              height: { xs: '288px', sm: '187px', md: '368px' },
+              position: 'relative', backgroundColor: '#6D6474', overflow: 'hidden'
+            }}
+          >
+            <CardMedia
+              component="img"
+              src="img/categories/category-tablets.png"
+              sx={{position:'relative', left: { xs: '17px', sm: '10px', md: '49px' }, top: { xs: '23.1px', sm: '20px', md: '40px' },
+              width: { xs: '427.3px', sm: '277.45px', md: '546px' },
+              height: { xs: '427.3px', sm: '277.45px', md: '546px' }}}
+            />
+          </Box>
+          <Box 
+            sx={{width: { xs: '288px', sm: '187px', md: '368px' },
+              height: { xs: '288px', sm: '187px', md: '368px' },
+              position: 'relative', backgroundColor: '#6D6474', overflow: 'hidden'
+            }}
+          >
+            <CardMedia
+              component="img"
+              src="img/categories/category-accesories.png"
+              sx={{position:'relative', left: { xs: '-289.65px', sm: '-189px', md: '-368px' }, top: { xs: '-355.9px', sm: '-218px', md: '-439px' },
+              width: { xs: '1152px', sm: '748px', md: '1472px' },
+              height: { xs: '688.7px', sm: '447.17px', md: '880px' }}}
+            />  
+          </Box>
+        </Box>
       </Section>
     </>
   );
