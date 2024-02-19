33b2cdd60686dcf86bf515bc687d23cfbecbe8e4
Timi2424 <tymonbieganski@wp.pl>
2024-02-19 12:48:39 +0100

    Added carousel commponent to phonePage, fetched data from phone endpoint

--- my-app/src/utils/fetchHelper.ts
+++ my-app/src/utils/fetchHelper.ts
@@ -11,3 +11,8 @@ export const getPhone = async (phoneId: string) => {
   const response = await axios.get(`https://phone-catalog-f9j4.onrender.com/phones/${phoneId}`);
   return response.data;
 };
+
+export const getRecommended = async (phoneId: string) => {
+  const response = await axios.get(`https://phone-catalog-f9j4.onrender.com/phones/${phoneId}/recommended`);
+  return response.data;
+};
