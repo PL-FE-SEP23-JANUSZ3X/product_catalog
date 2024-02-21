47f03512eda8f61cc6bfee59b9d1cb0a63777d15
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-02-21 10:54:14 +0100

    feat: add missing props (length and width)

--- my-app/src/components/skeletonLoader/SkeletonLoader.tsx
+++ my-app/src/components/skeletonLoader/SkeletonLoader.tsx
@@ -1,23 +1,56 @@
-import { Box, Grid, Skeleton, Theme, useMediaQuery } from "@mui/material";
+// import { Box, Grid, Skeleton, Theme, useMediaQuery } from "@mui/material";
 
-const SkeletonLoader = () => {
+// const SkeletonLoader = () => {
+//   const isTablet = useMediaQuery((theme: Theme)  => theme.breakpoints.down('md'));
+//   const isMobile = useMediaQuery((theme: Theme)  => theme.breakpoints.down('sm'));
+  
+//   let numberOfSkeletons = 4;
+//   let skeletonWidth = 272;
+
+//   if (isMobile) {
+//     numberOfSkeletons = 1;
+//     skeletonWidth = 288;
+//   } else if (isTablet) {
+//     numberOfSkeletons = 2;
+//     skeletonWidth = 288;
+//   }
+
+//   return (
+//     <Box sx={{ display: 'flex', gap: '20px' }}>
+//       {Array.from({ length: numberOfSkeletons }).map((_, index) => (
+//         <Grid key={index} item>
+//           <Skeleton variant="rounded" width={skeletonWidth} height={506} />
+//         </Grid>
+//       ))}
+//     </Box>
+//   );
+// };
+
+// export default SkeletonLoader;
+
+import { Box, Grid, Skeleton, Theme, useMediaQuery } from "@mui/material";
+import SkeletonLoaderProps from "./SkeletonLoader.types";
+
+const SkeletonLoader: React.FC<SkeletonLoaderProps> = ({ length, width }) => {
   const isTablet = useMediaQuery((theme: Theme)  => theme.breakpoints.down('md'));
   const isMobile = useMediaQuery((theme: Theme)  => theme.breakpoints.down('sm'));
   
-  let numberOfSkeletons = 4;
-  let skeletonWidth = 272;
+  let skeletonWidth = width.large;
+  let skeletonLength = length.large
 
   if (isMobile) {
-    numberOfSkeletons = 1;
-    skeletonWidth = 288;
-  } else if (isTablet) {
-    numberOfSkeletons = 2;
-    skeletonWidth = 288;
+    skeletonWidth = width.small;
+    skeletonLength = length.small;
+  } 
+
+  if (isTablet) {
+    skeletonWidth = width.medium;
+    skeletonLength = length.medium;
   }
 
   return (
     <Box sx={{ display: 'flex', gap: '20px' }}>
-      {Array.from({ length: numberOfSkeletons }).map((_, index) => (
+      {Array.from({ length: skeletonLength }).map((_, index) => (
         <Grid key={index} item>
           <Skeleton variant="rounded" width={skeletonWidth} height={506} />
         </Grid>
