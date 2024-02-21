6ce5d601704f7c93a2d951ff85a832d2dc835a8b
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-02-21 10:56:35 +0100

    fix: delete usused comments

--- my-app/src/components/skeletonLoader/SkeletonLoader.tsx
+++ my-app/src/components/skeletonLoader/SkeletonLoader.tsx
@@ -1,33 +1,3 @@
-// import { Box, Grid, Skeleton, Theme, useMediaQuery } from "@mui/material";
-
-// const SkeletonLoader = () => {
-//   const isTablet = useMediaQuery((theme: Theme)  => theme.breakpoints.down('md'));
-//   const isMobile = useMediaQuery((theme: Theme)  => theme.breakpoints.down('sm'));
-  
-//   let numberOfSkeletons = 4;
-//   let skeletonWidth = 272;
-
-//   if (isMobile) {
-//     numberOfSkeletons = 1;
-//     skeletonWidth = 288;
-//   } else if (isTablet) {
-//     numberOfSkeletons = 2;
-//     skeletonWidth = 288;
-//   }
-
-//   return (
-//     <Box sx={{ display: 'flex', gap: '20px' }}>
-//       {Array.from({ length: numberOfSkeletons }).map((_, index) => (
-//         <Grid key={index} item>
-//           <Skeleton variant="rounded" width={skeletonWidth} height={506} />
-//         </Grid>
-//       ))}
-//     </Box>
-//   );
-// };
-
-// export default SkeletonLoader;
-
 import { Box, Grid, Skeleton, Theme, useMediaQuery } from "@mui/material";
 import SkeletonLoaderProps from "./SkeletonLoader.types";
 
