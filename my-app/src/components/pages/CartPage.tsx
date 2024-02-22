ffd86dfc2067b38010fa1bd8890ecb7a9fe5cfb2
Katsiaryna Prakofyeva <catherine.smiff@gmail.com>
2024-02-22 19:40:16 +0100

    fix: added dollar symbol

--- my-app/src/components/pages/CartPage.tsx
+++ my-app/src/components/pages/CartPage.tsx
@@ -157,7 +157,7 @@ const CartPage = () => {
               }
               </Stack>
               <Stack sx={totalStyle} flexDirection="column" alignItems="center">
-                <Typography variant="h2">{total}</Typography>
+                <Typography variant="h2">{'$' + total}</Typography>
                 <Typography variant="body1" sx={{ mb: {xs: 2, md: 3}}}>
                     {`Total for ${order.length} items`}
                 </Typography>
